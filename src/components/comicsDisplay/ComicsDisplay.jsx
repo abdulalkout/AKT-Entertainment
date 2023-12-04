0c829f826f707aacfea858798621a1654b827a10
abdulalkout <abdul.alkout@gmail.com>
2023-12-04 14:02:34 -0600

    adding styles for comics card

--- src/components/comicsDisplay/ComicsDisplay.jsx
+++ src/components/comicsDisplay/ComicsDisplay.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import "./ComicsDisplay.css";
 import { MarvelApiContext } from "../../contexts/apiContext/MarvelApiContext";
@@ -6,15 +6,43 @@ import { MarvelApiContext } from "../../contexts/apiContext/MarvelApiContext";
 function ComicsDisplay() {
   const { currentComic } = useContext(MarvelApiContext);
   let marvelComicPic = `${currentComic.thumbnail.path}.${currentComic.thumbnail.extension}`;
+  const [creators, setCreators] = useState(false);
+
+  const openCreator = () => {
+    return currentComic.creators.items.map((creator, index) => {
+      return (
+        <p
+          onClick={showCreator}
+          className="comic-creators"
+          key={index}
+        >{`${creator.name}  Role: ${creator.role}`}</p>
+      );
+    });
+  };
+
+  const showCreator = () => {
+    setCreators(!creators);
+  };
+
   return (
-    <div>
-      <img src={marvelComicPic} />
-      <p>{currentComic.title}</p>
-      {currentComic.creators.items.map((creator, index) => {
-        return <p key={index}>{`${creator.name}  Role: ${creator.role}`}</p>;
-      })}
-      <p>{currentComic.textObjects[0].text}</p>
-      <a href={currentComic.urls[0].url}>Go to Marvel Website</a>
+    <div className="comic-display">
+      <img className="comic-img" src={marvelComicPic} />
+      <div className="comic-info">
+        <p className="comic-title">{currentComic.title}</p>
+        <p>{currentComic.textObjects[0].text}</p>
+
+        {creators ? (
+          openCreator()
+        ) : (
+          <p onClick={showCreator} className="see-creator">
+            see creators
+          </p>
+        )}
+
+        <a className="marvel-website" href={currentComic.urls[0].url}>
+          Go to Marvel Website
+        </a>
+      </div>
     </div>
   );
 }
