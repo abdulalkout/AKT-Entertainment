cad10a907723782b1325d717b6fa65ff5ff9a367
abdulalkout <abdul.alkout@gmail.com>
2023-12-06 10:34:38 -0600

    signIn feature to control the watching comics done

--- src/components/watch/WatchList.jsx
+++ src/components/watch/WatchList.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./WatchList.css";
 import { useState, useContext } from "react";
-import { MarvelApiContext } from "../../contexts/apiContext/MarvelApiContext";
+import { SignInContext } from "../../contexts/userContext/SignInContext";
 import WatchCard from "./WatchCard";
 
 function WatchList({ data }) {
+  const { signIn } = useContext(SignInContext);
   return (
     <div>
-      <p className="watch-header">Watch Later</p>
-      <div className="watch-list">
-        {data.map((item, index) => {
-          return <WatchCard key={index} item={item} />;
-        })}
-      </div>
+      {signIn ? (
+        <>
+          <p className="watch-header">Watch Later</p>
+          <div className="watch-list">
+            {data.map((item, index) => {
+              return <WatchCard key={index} item={item} />;
+            })}
+          </div>
+        </>
+      ) : null}
     </div>
   );
 }
