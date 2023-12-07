709d1299654c2ab57c925cf03db8e23978c6f911
abdulalkout <abdul.alkout@gmail.com>
2023-12-06 20:58:41 -0600

    styling the game display and making on click functionalty

--- src/components/homePageGame/HomePageGame.jsx
+++ src/components/homePageGame/HomePageGame.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomePageGame.css";
 import { useContext } from "react";
 import { GamesContext } from "../../contexts/apiContext/gamesContext";
 
 function HomePageGame() {
   const { games } = useContext(GamesContext);
+  const [onHover, setOnHover] = useState(".game-name-none");
+
+  const openGameUrl = () => {
+    window.open(games[0].url, "_blank");
+  };
 
   return (
     <div className="game-section">
-      <p className="game-name">{games[0].name}</p>
-      <img className="game-image" src={games[0].image} />
+      <p className="games-header">Game Time</p>
+      <div className="game-card" onClick={openGameUrl}>
+        <p
+          className={onHover}
+          onMouseLeave={() => {
+            setOnHover("game-name-none");
+          }}
+        >
+          {games[0].name}
+        </p>
+        <img
+          className="game-image"
+          src={games[0].image}
+          onMouseEnter={() => {
+            setOnHover("game-name");
+          }}
+        />
+      </div>
     </div>
   );
 }
