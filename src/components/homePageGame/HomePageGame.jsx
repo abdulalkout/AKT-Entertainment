a59de2f6ee35b796374fde484775742b933622ad
abdulalkout <abdul.alkout@gmail.com>
2023-12-06 20:33:55 -0600

    brought the game context data

--- src/components/homePageGame/HomePageGame.jsx
+++ src/components/homePageGame/HomePageGame.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import "./HomePageGame.css";
 import { useContext } from "react";
 import { GamesContext } from "../../contexts/apiContext/gamesContext";
-import tishTisha from "../../images/Tish-Tisha.jpg";
 
 function HomePageGame() {
   const { games } = useContext(GamesContext);
 
   return (
-    <div>
-      <p>sssssssssss</p>
-      <img src={tishTisha} />
+    <div className="game-section">
+      <p className="game-name">{games[0].name}</p>
+      <img className="game-image" src={games[0].image} />
     </div>
   );
 }
