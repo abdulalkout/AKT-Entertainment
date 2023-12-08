71ad85d3d88ab7e96f48d8b5511ae3fd2fc72bac
abdulalkout <abdul.alkout@gmail.com>
2023-12-08 13:41:48 -0600

    done with user logs page

--- src/components/userLogs/UserLogs.jsx
+++ src/components/userLogs/UserLogs.jsx
@@ -11,9 +11,6 @@ function UserLogs() {
   const { setCurrentProduct } = useContext(ProductsContext);
   const { cart, setCart, totalPrice } = useContext(SignInContext);
   const { watchLater, setWatchLater } = useContext(WatchContext);
-  const [display, setDisplay] = useState({
-    watchButton: "delet-watch",
-  });
 
   const deletItem = (item) => {
     const newCart = cart.filter((filteredItem) => filteredItem.id !== item.id);
@@ -22,51 +19,66 @@ function UserLogs() {
 
   const deletWatchList = () => {
     setWatchLater([]);
-    setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      [watchButton]: "",
-    }));
   };
 
   return (
     <>
       <div>
         <p className="cart-title">Cart Items</p>
-        <p className="price-tag">Total Price: ${totalPrice}</p>
-        <div className="suggestions">
-          {cart.map((item, index) => {
-            return (
-              <div key={index}>
-                <Link
-                  key={index}
-                  to="/ShopItem"
-                  onClick={() => {
-                    setCurrentProduct(item);
-                  }}
-                >
-                  <img className="suggestions-img" src={item.thumbnailImage} />
-                </Link>
-                <div className="item-info">
-                  <p>Price: ${item.basePrice}</p>
-                  <p>Items: {item.itemsNumber}</p>
-                  <button
-                    onClick={() => {
-                      deletItem(item);
-                    }}
-                    className="delet-item"
-                  >
-                    Delet Item
-                  </button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        {cart.length ? (
+          <>
+            <p className="price-tag">Total Price: ${totalPrice}</p>
+            <div className="suggestions">
+              {cart.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <Link
+                      key={index}
+                      to="/ShopItem"
+                      onClick={() => {
+                        setCurrentProduct(item);
+                      }}
+                    >
+                      <img
+                        className="suggestions-img"
+                        src={item.thumbnailImage}
+                      />
+                    </Link>
+
+                    <div className="item-info">
+                      <p>Price: ${item.basePrice}</p>
+                      <p>Items: {item.itemsNumber}</p>
+                      <button
+                        onClick={() => {
+                          deletItem(item);
+                        }}
+                        className="delet-item"
+                      >
+                        Delet Item
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <div className="empty-cart-div">
+            <p className="empty-cart-title">Your cart is empty</p>
+            <Link className="shop-now-lik" to="/Shopping">
+              Shop Now
+            </Link>
+          </div>
+        )}
       </div>
-      {watchLater.length ? <WatchList data={watchLater} /> : null}
-      <button onClick={deletWatchList} className="delet-watch">
-        Delete History
-      </button>
+      {watchLater.length ? (
+        <div>
+          <WatchList data={watchLater} />
+          <button onClick={deletWatchList} className="delet-watch ">
+            Delete History
+          </button>
+        </div>
+      ) : null}
     </>
   );
 }
