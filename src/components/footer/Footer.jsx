8c9a0fbcdacb73f3555d421f58d57492459be417
abdulalkout <abdul.alkout@gmail.com>
2023-12-10 21:16:45 -0600

    fixing the footer img and CSS

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,12 +3,30 @@ import "./Footer.css";
 import { Link } from "react-router-dom";
 
 function Footer() {
+  const openFootersLinks = (link) => {
+    window.open(link, "_blank");
+  };
+
   return (
     <div className="footer-div">
       <div className="footer-link">
         <Link to="/profile" className="footer-name">
           Who is Abdul ??
         </Link>
+        <div className="footer-pics-div">
+          <img
+            onClick={() =>
+              openFootersLinks(`https://www.linkedin.com/in/abdul-alkout/`)
+            }
+            className="footer-pics"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3236130/linkedin-outline-icon-sm.png"
+          />
+          <img
+            onClick={() => openFootersLinks(`https://github.com/abdulalkout`)}
+            className="footer-pics"
+            src="https://static-00.iconduck.com/assets.00/github-icon-2048x2048-91rgqivh.png"
+          />
+        </div>
       </div>
     </div>
   );
